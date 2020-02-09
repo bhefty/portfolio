9784c49d70d22263553e9b67aa8e57400f53a797
Bill Hefty <billhefty@gmail.com>
2020-02-09 10:09:21 -0600

    Update with own name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bill Hefty`,
+    description: `Front-End developer based in Austin, TX.`,
+    author: `@billhefty`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
